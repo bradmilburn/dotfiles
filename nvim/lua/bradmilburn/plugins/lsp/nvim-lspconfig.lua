9cb3b3378ea96d8e828e1a8865f81f25f0893ad3
Brad Milburn <brad@applicable.co.nz>
2024-07-24 19:51:12 +1200

    Add phpactor

--- nvim/lua/bradmilburn/plugins/lsp/nvim-lspconfig.lua
+++ nvim/lua/bradmilburn/plugins/lsp/nvim-lspconfig.lua
@@ -159,5 +159,15 @@ return {
         },
       },
     })
+
+    -- configure phpactor server
+    lspconfig["phpactor"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+      init_options = {
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
+      },
+    })
   end,
 }
